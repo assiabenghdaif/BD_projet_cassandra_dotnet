e5810cf1218cd3022fda352ef04d561f034f2dd6
assiabenghdaif <99361390+assiabenghdaif@users.noreply.github.com>
2023-12-23 10:04:14 +0100

    create projet

---
+++ BD_Projet_v-1-0-0/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BD_Projet_v_1_0_0.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
